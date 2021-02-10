fc152197a1a6287e6bf51cdbca0472faf0f455d9
yosuke nakai <74998898+skrk44@users.noreply.github.com>
2021-02-10 20:29:31 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+var VueTyperPlugin = window.VueTyper.default
+Vue.use(VueTyperPlugin)
+
+const targetElement = document.querySelectorAll(".animationTarget");
+console.log("画面の高さ",window.innerHeight)
+document.addEventListener("scroll",function() {
+  for (let i = 0; i < targetElement.length; i++) {
+    const getElementDistance = targetElement[i].getBoundingClientRect().top +  targetElement[i].clientHeight * 0.6
+    if (window.innerHeight > getElementDistance) {
+      targetElement[i].classList.add("show");
+    }
+  }
+})
+
+
